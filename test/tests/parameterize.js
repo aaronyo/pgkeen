ff984724cb1757c02ae059267832a1b9d7c0533a
Aaron Boyd <aaron.t.boyd@gmail.com>
2017-07-03 10:21:21 -0700

    curry parameterize

--- test/tests/parameterize.js
+++ test/tests/parameterize.js
@@ -38,7 +38,7 @@ suite('SQL Strings', () => {
 
   test('Replace params', async () => {
     assert.deepEqual(
-      parameterize('SELECT 1 FROM foo WHERE val = :val')({ val: 1 }),
+      parameterize('SELECT 1 FROM foo WHERE val = :val', { val: 1 }),
       {
         text: 'SELECT 1 FROM foo WHERE val = $1',
         values: [1],
@@ -46,11 +46,22 @@ suite('SQL Strings', () => {
     );
   });
 
+  test('Replace params, curried', async () => {
+    const simpleQuery = parameterize('SELECT 1 FROM foo WHERE val = :val');
+    assert(fp.isFunction(simpleQuery));
+
+    assert.deepEqual(simpleQuery({ val: 1 }), {
+      text: 'SELECT 1 FROM foo WHERE val = $1',
+      values: [1],
+    });
+  });
+
   test('Replace multiple params', async () => {
     assert.deepEqual(
       parameterize(
         'SELECT 1 FROM foo WHERE a = :a AND b = :b AND :a IS NOT NULL',
-      )({ a: 1, b: 2 }),
+        { a: 1, b: 2 },
+      ),
       {
         text: 'SELECT 1 FROM foo WHERE a = $1 AND b = $2 AND $1 IS NOT NULL',
         values: [1, 2],
@@ -60,7 +71,7 @@ suite('SQL Strings', () => {
 
   test('Ignore comments', async () => {
     assert.deepEqual(
-      parameterize('SELECT 1 FROM foo WHERE a = :a -- WHERE b = :a')({
+      parameterize('SELECT 1 FROM foo WHERE a = :a -- WHERE b = :a', {
         a: 1,
       }),
       {
@@ -72,7 +83,7 @@ suite('SQL Strings', () => {
 
   test('Ignore casts', async () => {
     assert.deepEqual(
-      parameterize('SELECT 1 FROM foo WHERE date = :date::date')({
+      parameterize('SELECT 1 FROM foo WHERE date = :date::date', {
         date: 1,
       }),
       {
