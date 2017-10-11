2a75e3f8a8e5c83aa97618f8efd93e987f6cb3d2
Aaron Boyd <aaron.t.boyd@gmail.com>
2017-10-10 22:03:30 -0700

    make the Promise implementation configurable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Client = require('./lib/client');
 const parameterize = require('./lib/parameterize').parameterize;
 const sqlFiles = require('./lib/sql-files');
+const { configure } = require('./lib/config');
 
-module.exports = { Client, parameterize, sqlFiles };
+module.exports = { Client, parameterize, sqlFiles, configure };
