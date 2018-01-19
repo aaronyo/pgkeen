b32cee209929c7484afc21fed15c6c4e2e2a7eb5
Aaron Boyd <aaron.t.boyd@gmail.com>
2018-01-19 00:29:38 -0800

    fix unreliable synchronize test

--- test/tests/util.js
+++ test/tests/util.js
@@ -10,16 +10,21 @@ const { Client, synchronized } = pgkeen;
 
 suite('Util', () => {
   test('Synchronized', async () => {
-    const dbClient = new Client({ pool: { max: 3 } });
+    const dbClient = new Client({ pool: { max: 10 } });
     const completionMarkers = [];
     await Promise.all([
       synchronized(dbClient, 'lockA', async () => {
-        await Promise.delay(50);
+        await Promise.delay(200);
         completionMarkers.push(2);
       }),
-      synchronized(dbClient, 'lockA', () => {
-        completionMarkers.push(3);
-        return Promise.resolve();
+      // There's no guarantee as to which statement would obtain 'lockA'
+      // first, so we stick a delay here to make it much more likely that
+      // the first statement will.
+      Promise.delay(10).then(() => {
+        synchronized(dbClient, 'lockA', () => {
+          completionMarkers.push(3);
+          return Promise.resolve();
+        });
       }),
       synchronized(dbClient, 'lockB', () => {
         completionMarkers.push(1);
