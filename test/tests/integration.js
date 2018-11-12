b9fbfc9c31d57ea6a1a9f9f40e3ed90d3751db38
Aaron Boyd <aaron.t.boyd@gmail.com>
2018-11-11 16:44:30 -0800

    add namedParamsFunc()

--- test/tests/integration.js
+++ test/tests/integration.js
@@ -10,6 +10,7 @@ const keen = require('../../index');
 const assert = require('assert');
 
 const func = fp.partial(keen.func, [keen.query]);
+const namedParamsFunc = fp.partial(keen.namedParamsFunc, [keen.query]);
 
 suite('Integration', () => {
   function createTestTable() {
@@ -25,35 +26,38 @@ suite('Integration', () => {
     await createTestTable();
   });
 
-  test('Use a sql file', async () => {
+  test('Use various query functions thar are bound to a pool', async () => {
+    // Create functions that perform db queries. We can build these functions
+    // in a veriety of ways, but note that they all expect a plain old node-pg
+    // client as their first argument.
     async function insert(pgClient, val) {
       await pgClient.query('INSERT INTO foo VALUES($1)', [val]);
     }
 
     const values = func('SELECT * from foo', keen.toScalars);
 
-    async function count(pgClient, { val }) {
-      return keen.toScalar(
-        await pgClient.query(
-          ...keen.toBindVars(
-            keen.readFileSync(__dirname, 'sql', 'count_foo.sql'),
-            { val },
-          ),
-        ),
-      );
-    }
+    const count = namedParamsFunc(
+      await keen.readFileSync(__dirname, 'sql', 'count_foo.sql'),
+      keen.toScalar,
+    );
 
     async function badConversion(pgClient) {
       return keen.toScalar(await pgClient.query('SELECT * from FOO'));
     }
 
+    // Make a pool of node pg clients
     const pool = keen.makePool(pg.Client);
+
+    // Create versions of the functions that will automatically be called
+    // against a client from the pool
     const bound = keen.bindAllToPool(pool, {
       insert,
       count,
       values,
       badConversion,
     });
+
+    // Now we can use the bound functions
     await bound.insert(1);
     await bound.insert(1);
     assert.equal(await bound.count({ val: 1 }), 2);
@@ -63,7 +67,8 @@ suite('Integration', () => {
     try {
       await bound.badConversion(1);
       throw new Error('should have failed');
-    } catch (e) {
+    } catch (err) {
+      assert(fp.startsWith('Expected 0 or 1 row', err.message));
       conversionFailed = true;
     }
     assert(conversionFailed);
